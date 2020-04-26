14354ef70f619bef1f8841e6587e6ec131736fb9
Bob Jarvis <bob@zeal.co>
2020-04-26 15:10:05 +0930

    added state machine

--- public/script.js
+++ public/script.js
@@ -1,114 +1,167 @@
+//CUBE DEFINITION
+function Cube() {
+  this.x = -2;
+  this.y = -2;
+  this.z = 0;
+  this._fsm(); //init statemachine
+}
+
+Cube.prototype = {
+  announce: function () {
+    console.log(
+      "pos is " +
+        this.x +
+        " " +
+        this.y +
+        " " +
+        this.z +
+        ", state is  " +
+        this.state
+    );
+  },
+};
+
+StateMachine.factory(Cube, {
+  init: "unselected",
+  transitions: [
+    { name: "makeHead", from: ["unselected", "body", "legs"], to: "head" },
+    { name: "makeBody", from: ["unselected", "head", "legs"], to: "body" },
+    { name: "makeLegs", from: ["unselected", "head", "body"], to: "legs" },
+    {
+      name: "makeUnselected",
+      from: ["head", "body", "legs"],
+      to: "unselected",
+    },
+  ],
+  methods: {
+    onMakeHead: function () {
+      console.log("made into head");
+    },
+    onMakeBody: function () {
+      console.log("made into body");
+    },
+    onMakeLegs: function () {
+      console.log("made into legs");
+    },
+    onMakeUnselected: function () {
+      console.log("made into unselected");
+    },
+  },
+});
 
 //SCENE
 var scene = new THREE.Scene();
 
-
 //CAMERA
-var camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
+var camera = new THREE.PerspectiveCamera(
+  75,
+  window.innerWidth / window.innerHeight,
+  0.1,
+  1000
+);
 camera.position.z = 3;
 
-
 //RENDERER
-var renderer = new THREE.WebGLRenderer( {alpha: true} );
-renderer.setSize( window.innerWidth, window.innerHeight );
-renderer.setClearColor( 0x000000, 0 ); // the default
+var renderer = new THREE.WebGLRenderer({ alpha: true });
+renderer.setSize(window.innerWidth, window.innerHeight);
+renderer.setClearColor(0x000000, 0); // the default
+document.body.appendChild(renderer.domElement);
 
-document.body.appendChild( renderer.domElement );
-
-
+//GEOMETRY
 //boxes
-var cubetop = new THREE.Mesh( 
-  new THREE.BoxGeometry(), 
-  new THREE.MeshStandardMaterial( { color: 0xaaaaaa } ) 
+var cubetop = new THREE.Mesh(
+  new THREE.BoxGeometry(),
+  new THREE.MeshStandardMaterial({ color: 0xaaaaaa })
 );
 // cubetop.castShadow = true;
 cubetop.position.y = 1.1;
-scene.add( cubetop );
+scene.add(cubetop);
 
-
-var cubemiddle = new THREE.Mesh( 
-  new THREE.BoxGeometry(), 
-  new THREE.MeshStandardMaterial( { color: 0xaaaaaa } ) 
+var cubemiddle = new THREE.Mesh(
+  new THREE.BoxGeometry(),
+  new THREE.MeshStandardMaterial({ color: 0xaaaaaa })
 );
 
 cubemiddle.position.y = 0;
-scene.add( cubemiddle );
+scene.add(cubemiddle);
 
-
-var cubebottom = new THREE.Mesh( 
-  new THREE.BoxGeometry(), 
-  new THREE.MeshStandardMaterial( { color: 0xaaaaaa } ) 
+var cubebottom = new THREE.Mesh(
+  new THREE.BoxGeometry(),
+  new THREE.MeshStandardMaterial({ color: 0xaaaaaa })
 );
 
 cubebottom.position.y = -1.1;
-scene.add( cubebottom );
+scene.add(cubebottom);
 
 var cubes = [cubetop, cubemiddle, cubebottom];
 
-
 //LIGHTS
-var ambientLight = new THREE.AmbientLight( 0x404040 );
+var ambientLight = new THREE.AmbientLight(0x404040);
 scene.add(ambientLight);
 
-var directionalLightRight = new THREE.DirectionalLight( 0xffcccc, 1 );
-directionalLightRight.position.set( 200, 350, 250 );
+var directionalLightRight = new THREE.DirectionalLight(0xffcccc, 1);
+directionalLightRight.position.set(200, 350, 250);
 // directionalLightRight.castShadow = true;
-scene.add( directionalLightRight );
+scene.add(directionalLightRight);
 
-var directionalLightLeft = new THREE.DirectionalLight( 0xccccff, 1 );
-directionalLightLeft.position.set( -200, 350, 250 );
+var directionalLightLeft = new THREE.DirectionalLight(0xccccff, 1);
+directionalLightLeft.position.set(-200, 350, 250);
 // directionalLightLeft.castShadow = true;
-scene.add( directionalLightLeft );
-
+scene.add(directionalLightLeft);
 
 //RAYCASTER
 var raycaster = new THREE.Raycaster();
 var mouse = new THREE.Vector2();
-mouse.x, mouse.y = -2;
+mouse.x, (mouse.y = -2);
 
-
-function onMouseMove( event ) {
-	// calculate mouse position in normalized device coordinates
-	// (-1 to +1) for both components
-
-	mouse.x = ( event.clientX / window.innerWidth ) * 2 - 1;
-	mouse.y = - ( event.clientY / window.innerHeight ) * 2 + 1;
+function onMouseMove(event) {
+  // calculate mouse position in normalized device coordinates
+  // (-1 to +1) for both components
+  mouse.x = (event.clientX / window.innerWidth) * 2 - 1;
+  mouse.y = -(event.clientY / window.innerHeight) * 2 + 1;
 }
 
-
-
-function onWindowResize(event){
+function onWindowResize(event) {
   camera.aspect = window.innerWidth / window.innerHeight;
   camera.updateProjectionMatrix();
-  renderer.setSize( window.innerWidth, window.innerHeight );
+  renderer.setSize(window.innerWidth, window.innerHeight);
 }
 
+function checkForHover() {
+  // update the picking ray with the camera and mouse position
+  raycaster.setFromCamera(mouse, camera);
 
-function checkForHover(){
-	// update the picking ray with the camera and mouse position
-	raycaster.setFromCamera( mouse, camera );
+  // calculate objects intersecting the picking ray
+  var intersects = raycaster.intersectObjects(scene.children);
 
-	// calculate objects intersecting the picking ray
-	var intersects = raycaster.intersectObjects( scene.children );
-    
-  cubes.map(cube => { 
+  //reset all colours
+  cubes.map((cube) => {
     cube.material.color.set(0xaaaaaa);
   });
 
-	for ( var i = 0; i < intersects.length; i++ ) {
-    intersects[ i ].object.material.color.set( 0xff00ff );
-	}
+  //set picked cubes to pink
+  for (var i = 0; i < intersects.length; i++) {
+    intersects[i].object.material.color.set(0xff00ff);
+  }
 }
 
 
+//-----------------------------------------------
+//test vars
+var testCube = new Cube();
+testCube.announce();
+testCube.makeHead();
+testCube.announce();
+
+
+//-----------------------------------------------
+//MAIN RENDER
 function render() {
-
   checkForHover();
-  renderer.render( scene, camera );
-  requestAnimationFrame( render );
-
+  renderer.render(scene, camera);
+  requestAnimationFrame(render);
 }
 
-window.addEventListener( 'mousemove', onMouseMove, false );
-window.addEventListener('resize', onWindowResize, false);
+window.addEventListener("mousemove", onMouseMove, false);
+window.addEventListener("resize", onWindowResize, false);
 window.requestAnimationFrame(render);
